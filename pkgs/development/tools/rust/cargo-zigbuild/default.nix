fa76cb3f49d28936d1cc78dab4d553edfcb93339
Chris Pick <chris@chrispick.com>
2024-04-17 13:03:25 -0400

    Fix `cargo-zigbuild test`s pass-through arguments
    Pull in unreleased fix to:
    https://github.com/rust-cross/cargo-zigbuild/issues/240

--- pkgs/development/tools/rust/cargo-zigbuild/default.nix
+++ pkgs/development/tools/rust/cargo-zigbuild/default.nix
@@ -7,11 +7,11 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "messense";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-wL6Rmw5hJI8cJDw2WO9CDOyeOuZv6QoFxrn81JrYBR4=";
+    rev = "8a76c1dbb0c6c704af599652d99545fde2ba6719";
+    hash = "sha256-+/KPa+0AQZrww+ua70B3luzYywivFINTowBsNH7doQs=";
   };
 
-  cargoHash = "sha256-uCZYDh4+Pri77DzqZj12cav7o8eDY2+fgwIwVBdcbHg=";
+  cargoHash = "sha256-wQyD6hP6yDIEP+utr8Gcd9I1h/E750pFP1/03niR6oI=";
 
   nativeBuildInputs = [ makeWrapper ];
 
